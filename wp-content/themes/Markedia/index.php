8d8d9730923213db939534fd8e1d109c2c7f29a2
vovaivanitskyi <vovaivanitskyi@gmail.com>
2023-06-18 17:23:15 +0300

    create and add pagination functionality

--- wp-content/themes/Markedia/index.php
+++ wp-content/themes/Markedia/index.php
@@ -52,16 +52,7 @@
 
                     <div class="row">
                         <div class="col-md-12">
-                            <nav aria-label="Page navigation">
-                                <ul class="pagination justify-content-center">
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">Next</a>
-                                    </li>
-                                </ul>
-                            </nav>
+                            <?php markedia_pagination() ?>
                         </div><!-- end col -->
                     </div><!-- end row -->
                 </div><!-- end col -->
