2f2fcbc722d9ccc05b87cc858ff3c874a117010f
vovaivanitskyi <vovaivanitskyi@gmail.com>
2023-06-28 15:28:49 +0300

    add header-banner sections

--- wp-content/themes/Markedia/functions.php
+++ wp-content/themes/Markedia/functions.php
@@ -25,13 +25,12 @@ function markedia_setup(){
 add_action('after_setup_theme', 'markedia_setup');
 
 function markedia_banner_customize_register($wp_customize) {
-    // Додайте секцію
-    $wp_customize->add_section('background_image_header_banner_section', array(
-        'title' => 'Background Image Banner Header',
+
+    $wp_customize->add_section('header_banner_section', array(
+        'title' => 'Banner Header',
         'priority' => 30
     ));
 
-    // Додайте поле для вибору зображення
     $wp_customize->add_setting('background_image_header_banner_setting', array(
         'default' => '',
         'sanitize_callback' => 'esc_url_raw'
@@ -39,10 +38,59 @@ function markedia_banner_customize_register($wp_customize) {
 
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'background_image_header_banner_control', array(
         'label' => 'Select an image',
-        'section' => 'background_image_header_banner_section',
+        'section' => 'header_banner_section',
         'settings' => 'background_image_header_banner_setting'
     )));
+
+    $wp_customize->add_setting('header_banner_title_setting', array(
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+
+    $wp_customize->add_control('header_banner_title_control', array(
+        'label' => 'Banner Title',
+        'section' => 'header_banner_section',
+        'settings' => 'header_banner_title_setting',
+        'type' => 'text'
+    ));
+
+    $wp_customize->add_setting('header_banner_description_setting', array(
+        'default' => '',
+        'sanitize_callback' => 'sanitize_textarea_field'
+    ));
+
+    $wp_customize->add_control('header_banner_description_control', array(
+        'label' => 'Banner Description',
+        'section' => 'header_banner_section',
+        'settings' => 'header_banner_description_setting',
+        'type' => 'textarea'
+    ));
+
+    $wp_customize->add_setting('header_banner_button_text_setting', array(
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+
+    $wp_customize->add_control('header_banner_button_text_control', array(
+        'label' => 'Button Text',
+        'section' => 'header_banner_section',
+        'settings' => 'header_banner_button_text_setting',
+        'type' => 'text'
+    ));
+
+    $wp_customize->add_setting('header_banner_button_link_setting', array(
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+
+    $wp_customize->add_control('header_banner_button_link_control', array(
+        'label' => 'Button Link',
+        'section' => 'header_banner_section',
+        'settings' => 'header_banner_button_link_setting',
+        'type' => 'text'
+    ));
 }
+
 add_action('customize_register', 'markedia_banner_customize_register');
 
 function wp_markedia_scripts(){
--- wp-content/themes/Markedia/template-parts/header-banner.php
+++ wp-content/themes/Markedia/template-parts/header-banner.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-12 align-self-center">
-                <h2>A digital marketing blog</h2>
-                <p class="lead"> Aenean ut hendrerit nibh. Duis non nibh id tortor consequat cursus at mattis felis. Praesent sed lectus et neque auctor dapibus in non velit. Donec faucibus odio semper risus rhoncus rutrum. Integer et ornare mauris.</p>
-                <a href="#" class="btn btn-primary">Try for free</a>
+                <h2><?php echo get_theme_mod('header_banner_title_setting')?></h2>
+                <p class="lead"><?php echo get_theme_mod('header_banner_description_setting')?></p>
+                <a href="<?php echo get_theme_mod('header_banner_button_link_setting')?>" class="btn btn-primary"><?php echo get_theme_mod('header_banner_button_text_setting')?></a>
             </div>
             <div class="col-lg-4 col-md-12">
                 <div class="newsletter-widget text-center align-self-center">
